d695be91d824bc6845746fae648fe11020cf84c9
mkauer <mkauer@icecube.wisc.edu>
2025-02-21 14:43:10 -0600

    tweak kickstart script

--- install-taxi.sh
+++ install-taxi.sh
@@ -7,5 +7,10 @@ fi
 
 host=$1
 rsync="/usr/bin/rsync --perms --times --recursive --checksum --partial --inplace --progress --timeout=120"
+
+# copy files over
 ${rsync} ./taxiXX/* ${host}:/
+
+# run the kickstart script
+ssh $host "/home/root/kickstart.sh"
 
