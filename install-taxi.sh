d69d2f4fb476f5276f11703f00e184b8fe2a360b
mkauer <mkauer@icecube.wisc.edu>
2025-02-21 15:23:01 -0600

    tweak install script

--- install-taxi.sh
+++ install-taxi.sh
@@ -4,9 +4,16 @@ if [ -z "$1" ]; then
     echo "specify a hostname"
     exit 1
 fi
-
 host=$1
+
+# rsync command and options to use
 rsync="/usr/bin/rsync --perms --times --recursive --checksum --partial --inplace --progress --timeout=120"
+
+# get some permissions fixed first (if from github)
+chmod go-w ./taxiXX/*
+chmod go-rwx ./taxiXX/home/root
+chmod -R go-rwx ./taxiXX/home/root/.ssh
+chmod u+x ./taxiXX/home/root/kickstart.sh
 
 # copy files over
 ${rsync} ./taxiXX/* ${host}:/
