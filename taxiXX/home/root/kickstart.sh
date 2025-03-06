11ba063f650ad8afd2f179e47f209c21dc09c073
mkauer <mkauer@icecube.wisc.edu>
2025-03-05 18:45:05 -0600

    tweak PATH

--- taxiXX/home/root/kickstart.sh
+++ taxiXX/home/root/kickstart.sh
@@ -4,7 +4,7 @@
 # M.Kauer
 version=2025-03-05
 
-echo $PATH
+
 source /home/root/.profile
 export PATH=/sbin:$PATH
 
@@ -19,18 +19,14 @@ chmod -R go-rwx /home/root/.ssh
 #------------------------------------------
 #FPGA="0x217_taxitop_icescint_06_11_2020_Taxi_3.1.bit"
 FPGA="taxiTop_iceScint_0x0301.bit"
-#FPGA="taxiTop_iceScint_0x0302.bit"  # Sasha said this one has issues
 rm /opt/taxi/firmware/default-firmware
 ln -sf /opt/taxi/firmware/${FPGA} /opt/taxi/firmware/default-firmware.bit
 ls -hAl /opt/taxi/firmware/default-firmware.bit
-#/opt/taxi/bin/fpgainit -f /opt/taxi/firmware/default-firmware.bit
-#/home/root/bin/firmware-version.sh
 
 
 # FieldHub Application
 #------------------------------------------
 #DIST="udaq-dist-jun_22_2023"
-#DIST="udaq-dist-nov_21_2024"
 DIST="udaq-dist-dec_26_2024"
 rm -r /opt/taxi/udaq
 ln -sf /opt/taxi/sfw/${DIST} /opt/taxi/udaq
@@ -38,12 +34,6 @@ ls -hAl /opt/taxi/udaq
 FIRM=`ls /opt/taxi/udaq/firmware`
 ln -sf /opt/taxi/udaq/firmware/${FIRM} /opt/taxi/udaq/firmware/firmware.bin
 ls -hAl /opt/taxi/udaq/firmware/firmware.bin
-
-
-# FieldHub kernel driver
-#------------------------------------------
-#/sbin/rmmod udaqdrv.ko
-#/sbin/insmod /opt/taxi/udaq/platform/udaqdrv.ko
 
 
 # Runtime init scripts for udaqdrv.ko and fpgaboot
